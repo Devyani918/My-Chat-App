cb04d505a010b7b0c3800c7ac15467f4fd3fb93c
Devyani Kumari <devyanik457@gmail.com>
2025-04-26 14:46:24 +0530

    socket

--- client/src/redux/userSlice.js
+++ client/src/redux/userSlice.js
@@ -1,46 +1,107 @@
+// import { createSlice } from "@reduxjs/toolkit";
+
+// const initialState = {
+//     _id : "",
+//     name : "",
+//     email : "",
+//     profile_pic : "",
+//     token : "",
+//     onlineUser : [],
+//     socketConnection : null 
+// }
+
+// export const userSlice = createSlice({
+//     name: 'user',
+//     initialState,
+//     reducers: {
+//         setUser : (state,action)=>{
+//             state._id = action.payload._id
+//             state.name = action.payload.name
+//             state.email = action.payload.email 
+//             state.profile_pic = action.payload.profile_pic
+//         },
+//         setToken : (state,action)=>{
+//             state.token = action.payload 
+//         },
+//         logout : (state,action)=>{
+//             state._id = ""
+//             state.name = ""
+//             state.email = ""
+//             state.profile_pic = ""
+//             state.token = ""
+//             state.socketConnection = null 
+//         },
+//         setOnlineUser : (state,action)=>{
+//             state.onlineUser = action.payload
+//         },
+//         setSocketConnection : (state,action)=>{
+//             state.socketConnection = action.payload  
+//         }
+//     }, 
+// })
+
+// //action creators are generated for each case reducer function
+// export const { setUser, setToken, logout, setOnlineUser, setSocketConnection } = userSlice.actions
+
+// export default userSlice.reducer; 
+
+
+
+//updated code
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-    _id : "",
-    name : "",
-    email : "",
-    profile_pic : "",
-    token : "",
-    onlineUser : [],
-    socketConnection : null 
-}
+  _id: "",
+  name: "",
+  email: "",
+  profile_pic: "",
+  token: "",
+  onlineUser: [],
+  socketConnection: null,
+  socketConnecting: false,  // New state for tracking socket connection status
+};
 
 export const userSlice = createSlice({
-    name: 'user',
-    initialState,
-    reducers: {
-        setUser : (state,action)=>{
-            state._id = action.payload._id
-            state.name = action.payload.name
-            state.email = action.payload.email 
-            state.profile_pic = action.payload.profile_pic
-        },
-        setToken : (state,action)=>{
-            state.token = action.payload 
-        },
-        logout : (state,action)=>{
-            state._id = ""
-            state.name = ""
-            state.email = ""
-            state.profile_pic = ""
-            state.token = ""
-            state.socketConnection = null 
-        },
-        setOnlineUser : (state,action)=>{
-            state.onlineUser = action.payload
-        },
-        setSocketConnection : (state,action)=>{
-            state.socketConnection = action.payload  
-        }
-    }, 
-})
+  name: "user",
+  initialState,
+  reducers: {
+    setUser: (state, action) => {
+      state._id = action.payload._id;
+      state.name = action.payload.name;
+      state.email = action.payload.email;
+      state.profile_pic = action.payload.profile_pic;
+    },
+    setToken: (state, action) => {
+      state.token = action.payload;
+    },
+    logout: (state, action) => {
+      state._id = "";
+      state.name = "";
+      state.email = "";
+      state.profile_pic = "";
+      state.token = "";
+      state.socketConnection = null;
+    },
+    setOnlineUser: (state, action) => {
+      state.onlineUser = action.payload;
+    },
+    setSocketConnection: (state, action) => {
+      state.socketConnection = action.payload;
+    },
+    setSocketConnecting: (state, action) => {
+      state.socketConnecting = action.payload;  // To manage the socket connection status
+    },
+  },
+});
 
-//action creators are generated for each case reducer function
-export const { setUser, setToken, logout, setOnlineUser, setSocketConnection } = userSlice.actions
+// Export action creators
+export const {
+  setUser,
+  setToken,
+  logout,
+  setOnlineUser,
+  setSocketConnection,
+  setSocketConnecting,  // Export the new action
+} = userSlice.actions;
 
-export default userSlice.reducer; 
+export default userSlice.reducer;
